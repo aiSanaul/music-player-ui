fc37948374ed730251004fc526f3a79ed7d0c8a9
aiSanaul <boyelarthur@gmail.com>
2024-09-20 17:07:31 +0530

    tailwind

---
+++ main.js
@@ -0,0 +1,15 @@
+function copyrights() {
+    pop = document.getElementById("crs");
+
+    if(pop.style.display = "none") {
+        pop.style.display = "flex";
+    }
+}
+
+function close() {
+    pop = document.getElementById("crs");
+
+    if(pop.style.display = "flex") {
+        pop.style.display = "none";
+    }
+}
