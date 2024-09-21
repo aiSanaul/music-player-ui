89b2654aea3255d66500267244ed314318c3e062
aiSanaul <boyelarthur@gmail.com>
2024-09-21 18:57:37 +0530

    Header Kinda Completed

--- main.js
+++ main.js
@@ -13,3 +13,13 @@ function close() {
         pop.style.display = "none";
     }
 }
+
+function play() {
+    document.getElementById("pause").style.display = "block";
+    document.getElementById("play").style.display = "none";
+}
+
+function pause() {
+    document.getElementById("play").style.display = "block";
+    document.getElementById("pause").style.display = "none";
+}
